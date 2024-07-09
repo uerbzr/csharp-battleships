82dd3055971d1800a25faaa11d83e8ec5d2e121d
uerbzr <nigel.sibbert@gmail.com>
2024-07-09 23:12:43 +0100

    updated

---
+++ exercise.wwwapi/Endpoints/BattleshipsEndpoint.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace exercise.wwwapi.Endpoints
+{
+    public static class BattleshipsEndpoint
+    {
+        public static void ConfigureBattleshipsEndpoints(this WebApplication app)
+        {
+            var battleships = app.MapGroup("battleships");
+
+            battleships.MapGet("/players", GetPlayers);
+        }
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public static async Task<IResult> GetPlayers()
+        {
+            return TypedResults.Ok("ok");
+        }
+    }
+}
